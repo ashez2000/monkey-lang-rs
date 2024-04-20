9f598f0114dd21a3936b8f07f8a396944f31a449
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2024-04-20 21:39:00 +0530

    Add tokens

---
+++ src/token/mod.rs
@@ -0,0 +1,32 @@
+pub enum TokenType {
+    Illegal,
+    Eof,
+
+    Int,
+    Assign,
+
+    Plus,
+    Minus,
+
+    Comma,
+    Semicolon,
+
+    LParen,
+    RParen,
+    LBrace,
+    RBrace,
+
+    Function,
+    Let,
+}
+
+pub struct Token {
+    pub ttype: TokenType,
+    pub literal: String,
+}
+
+impl Token {
+    pub fn new(ttype: TokenType, literal: String) -> Self {
+        Self { ttype,  literal}
+    }
+}
