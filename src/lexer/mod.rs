b2c05951c1a8c07b12d889d729e60a840dc62165
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2024-04-22 10:39:09 +0530

    Extend lexer

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -2,7 +2,7 @@ mod tests;
 
 use crate::token::*;
 
-#[derive(Default)]
+#[derive(Debug, Default)]
 pub struct Lexer {
     input: Vec<char>,
     position: usize,
@@ -19,18 +19,53 @@ impl Lexer {
     }
 
     pub fn next_token(&mut self) -> Token {
+        self.skip_whitespace();
+
         let tok = match self.ch {
-            '=' => new_token(TokenType::Assign, self.ch),
+            '=' => {
+                if self.peek_char() == '=' {
+                    self.read_char();
+                    Token::new(TokenType::Eq, "==".to_string())
+                } else {
+                    new_token(TokenType::Assign, self.ch)
+                }
+            }
+
+            '!' => {
+                if self.peek_char() == '=' {
+                    self.read_char();
+                    Token::new(TokenType::NotEq, "!=".to_string())
+                } else {
+                    new_token(TokenType::Bang, self.ch)
+                }
+            }
+
+            '+' => new_token(TokenType::Plus, self.ch),
+            '-' => new_token(TokenType::Minus, self.ch),
+            '*' => new_token(TokenType::Asterisk, self.ch),
+            '/' => new_token(TokenType::Slash, self.ch),
+            '<' => new_token(TokenType::Lt, self.ch),
+            '>' => new_token(TokenType::Gt, self.ch),
             ';' => new_token(TokenType::Semicolon, self.ch),
             '(' => new_token(TokenType::LParen, self.ch),
             ')' => new_token(TokenType::RParen, self.ch),
             ',' => new_token(TokenType::Comma, self.ch),
-            '+' => new_token(TokenType::Plus, self.ch),
             '{' => new_token(TokenType::LBrace, self.ch),
             '}' => new_token(TokenType::RBrace, self.ch),
 
             '\0' => new_token(TokenType::Eof, self.ch),
-            _ => new_token(TokenType::Illegal, self.ch),
+            _ => {
+                if is_letter(self.ch) {
+                    let literal = self.read_identifier();
+                    let tt = lookup_ident(&literal);
+                    Token::new(tt, literal)
+                } else if self.ch.is_numeric() {
+                    let number = self.read_number();
+                    Token::new(TokenType::Int, number)
+                } else {
+                    new_token(TokenType::Illegal, self.ch)
+                }
+            }
         };
 
         self.read_char();
@@ -47,8 +82,42 @@ impl Lexer {
         self.position = self.read_position;
         self.read_position += 1;
     }
+
+    fn peek_char(&self) -> char {
+        if self.read_position >= self.input.len() {
+            '\0'
+        } else {
+            self.input[self.read_position]
+        }
+    }
+
+    fn skip_whitespace(&mut self) {
+        while self.ch.is_ascii_whitespace() {
+            self.read_char();
+        }
+    }
+
+    fn read_identifier(&mut self) -> String {
+        let position = self.position;
+        while is_letter(self.ch) {
+            self.read_char();
+        }
+        self.input[position..self.position].iter().collect()
+    }
+
+    fn read_number(&mut self) -> String {
+        let position = self.position;
+        while self.ch.is_numeric() {
+            self.read_char();
+        }
+        self.input[position..self.position].iter().collect()
+    }
 }
 
 fn new_token(tt: TokenType, c: char) -> Token {
     Token::new(tt, c.to_string())
+}
+
+fn is_letter(ch: char) -> bool {
+    ch.is_alphabetic() || ch == '_'
 }
--- src/token/mod.rs
+++ src/token/mod.rs
@@ -3,11 +3,20 @@ pub enum TokenType {
     Illegal,
     Eof,
 
+    Ident,
     Int,
-    Assign,
 
+    // operators
+    Assign,
     Plus,
     Minus,
+    Bang,
+    Asterisk,
+    Slash,
+    Lt,
+    Gt,
+    Eq,
+    NotEq,
 
     Comma,
     Semicolon,
@@ -17,8 +26,14 @@ pub enum TokenType {
     LBrace,
     RBrace,
 
+    // keywords
     Function,
     Let,
+    True,
+    False,
+    If,
+    Else,
+    Return,
 }
 
 pub struct Token {
@@ -29,5 +44,19 @@ pub struct Token {
 impl Token {
     pub fn new(ttype: TokenType, literal: String) -> Self {
         Self { ttype, literal }
+    }
+}
+
+pub fn lookup_ident(ident: &str) -> TokenType {
+    match ident {
+        "fn" => TokenType::Function,
+        "let" => TokenType::Let,
+        "true" => TokenType::True,
+        "false" => TokenType::False,
+        "if" => TokenType::If,
+        "else" => TokenType::Else,
+        "return" => TokenType::Return,
+
+        _ => TokenType::Ident,
     }
 }
