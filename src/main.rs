c6ac585756b2e6404f4a95cff8b8f3681e1963ab
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2024-05-09 00:12:00 +0530

    Eval program from source file using args

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,37 @@ mod parser;
 mod repl;
 mod token;
 
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Welcome to Monkey Lang v0.1.0");
-    repl::start();
+    let args: Vec<_> = env::args().collect();
+
+    match args.len() {
+        1 => {
+            println!("Welcome to Monkey Lang v0.1.0.");
+            repl::start();
+        }
+
+        2 => {
+            let input = fs::read_to_string(&args[1]).unwrap();
+            let lexer = lexer::Lexer::new(&input);
+            let mut parser = parser::Parser::new(lexer);
+            let mut eval = evaluator::Evaluator::new();
+            let program = parser.parse_program().expect("error parsing program");
+            eval.eval_program(program);
+            if parser.get_errors().len() != 0 {
+                print_parse_errors(parser.get_errors());
+            }
+        }
+
+        _ => panic!("Invalid args len"),
+    }
+}
+
+fn print_parse_errors(errors: &Vec<String>) {
+    println!("ERROR:");
+    for e in errors {
+        println!("\t-> {}", e);
+    }
 }
